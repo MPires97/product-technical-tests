f4ce3af26d5295395f256175423d186706a85a70
MPires97 <ma2pires@gmail.com>
2022-07-23 13:10:59 +0100

    BoutiquesNearby: Added Comments

--- boutique-service/src/httpApi/v1/handlers/boutiquesNear.js
+++ boutique-service/src/httpApi/v1/handlers/boutiquesNear.js
@@ -8,15 +8,21 @@ export default function boutiques({models}, req, res, next) {
     const userLon = req?.query?.lon;
     var boutiquesCount = req?.query?.boutiquesCount || 5;
 
+    //Very basic checks
     if(check.checkFloat(userLat)
         && check.checkFloat(userLon)
         && check.checkInt(boutiquesCount)) {
+
             var closestBoutiques = [];
+
             Boutique.find({})
                 .then(boutiques => {
+                    //Checks the requested number of boutques is not longer than the actual number of boutiques
                     boutiquesCount = boutiquesCount > boutiques.length ? boutiques.length : boutiquesCount;
+                    
                     closestBoutiques = getClosestBoutique(boutiques, userLat, userLon, haversine, boutiquesCount);
                     res.send(closestBoutiques);
+
                 })
                 .catch(next);
         }
@@ -30,24 +36,44 @@ export default function boutiques({models}, req, res, next) {
 function getClosestBoutique(boutiques, userLat, userLon, distanceFunction, boutiquesCount){
 	const closestBoutiquesObj = [];
     var maxDist = Number.MAX_SAFE_INTEGER;
+
 	boutiques.forEach(boutique => {
+
+        //Computes distance between user and boutique
 	    var location = boutique.location;
 	    var distance = distanceFunction(userLat, userLon, location.lat, location.lon);
+        
+        /*
+        Only adds this boutique to array of closest boutiques if 
+        distance of this boutique is lower than the distance of the furthest
+        boutique in said array
+        */
         if(distance<maxDist){
             var boutiqueObj = new Object();
             boutiqueObj.distance = distance;
             boutiqueObj.boutique = boutique;
+            
+            /*
+            The array of closest boutiques is always sorted,
+            thus, we only need to worry about adding a boutique to it
+            in the correct index
+            */
             var index = sortedIndex(closestBoutiquesObj, distance);
             closestBoutiquesObj.splice(index,0,boutiqueObj);
+
             if(closestBoutiquesObj.length>boutiquesCount){
+                /*
+                if the amount of boutiques in the array of closest boutiques
+                is bigger than the requested amount (eg 5), 
+                then the furthest boutiques of that array is removed
+                */
                 closestBoutiquesObj.pop();
                 maxDist = closestBoutiquesObj[closestBoutiquesObj.length-1].distance;
             }
         }
 	});
-    const closestBoutiques = [];
-    closestBoutiquesObj.forEach(b=>closestBoutiques.push(b.boutique));
-	return closestBoutiques;
+
+	return closestBoutiquesObj;
 }
 
 
